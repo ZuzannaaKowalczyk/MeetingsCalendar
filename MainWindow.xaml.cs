5ebe7765556acdf7a8f502c7e1182dfeae294f29
zuzanna21 <zuzannakowalczyk02@gmail.com>
2024-03-12 10:55:38 +0100

    okna + kalendarz

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,15 @@ using System.Windows.Shapes;
 
 namespace Calendar
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
+
+
 }
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calendar.ViewModels
+{
+    public class LoginViewModel : BindableBase
+    {
+        
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set { SetProperty(ref username, value); }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        
+        public DelegateCommand LoginCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new DelegateCommand(Login);
+        }
+
+        private void Login()
+        {
+            //weryfikacja danych logowania
+
+            MainViewModel mainViewModel = new MainViewModel();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.DataContext = mainViewModel;
+            Application.Current.MainWindow.Close(); 
+            mainWindow.Show();
+        }
+    }
+}
