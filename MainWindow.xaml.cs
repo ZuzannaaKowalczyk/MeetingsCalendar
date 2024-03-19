ccd398330af709ac591a9b18bbb73632ba1981ab
zuzanna21 <zuzannakowalczyk02@gmail.com>
2024-03-19 09:57:53 +0100

    SQL, xaml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,10 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace Calendar
 {
- 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -23,7 +24,47 @@ namespace Calendar
             InitializeComponent();
         }
 
+        public class User
+        {
+            public int UserID { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public List<Events> List { get; set; } // Lista wydarzeń przypisanych do użytkownika
+        }
+
+        public class Kalendarz
+        {
+            public int IDkalendarza { get; set; }
+            public int UserID { get; set; }  
+        }
+
+        public class Events
+        {
+            public int CalendarID { get; set; }
+            public int UserID { get; set; } // ID użytkownika, do którego należy wydarzenie
+            public int EventID { get; set; } // ID wydarzenia
+            public string Title { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<User> Blogs { get; set; }
+            public DbSet<Events> Posts { get; set; }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
     }
+
+    
 
 
 }
---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId] INT IDENTITY (1, 1) NOT NULL,
+    [UserName] NVARCHAR (50) NOT NULL,
+    [Password]  NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
+
+CREATE TABLE [dbo].[Calendars] (
+    [CalendarId] INT IDENTITY (1, 1) NOT NULL,
+    [UserId] INT IDENTITY (1, 1) NOT NULL,
+
+    CONSTRAINT [PK_dbo.Calendars] PRIMARY KEY CLUSTERED ([CalendarId] ASC),
+    CONSTRAINT [FK_dbo.Calendars_dbo.Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [dbo].[Calendars] (
+    [CalendarId] INT IDENTITY (1, 1) NOT NULL,
+    [UserId] INT IDENTITY (1, 1) NOT NULL,
+
+    CONSTRAINT [PK_dbo.Calendars] PRIMARY KEY CLUSTERED ([CalendarId] ASC),
+    CONSTRAINT [FK_dbo.Calendars_dbo.Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId]) ON DELETE CASCADE
+);
